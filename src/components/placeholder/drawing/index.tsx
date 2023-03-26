af94e280c425a6e0ef063a6b0315e7d79816a3c7
Robert Todea <robert.todea@mai.ai>
2023-03-26 14:18:57 +0300

    Fix coordinate systems

--- src/components/placeholder/drawing/index.tsx
+++ src/components/placeholder/drawing/index.tsx
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from "react";
 
 import * as THREE from "three";
+import { Vector3 } from "three";
 import { Canvas, ThreeElements } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+// import { OrbitControls, OrthographicCamera } from "@react-three/drei";
 
 export const DrawingId = "drawing";
 
@@ -28,6 +29,56 @@ export const Square = (props: ThreeElements["mesh"]) => {
   );
 };
 
+/* 2D coordinate system */
+/*
+(0, 0)
+ -------> (x)
+ |
+ |
+ v
+ (y)
+ */
+
+/* 3D coordinate system */
+/* The X axis is red. The Y axis is green. The Z axis is blue. */
+/*
+ --------> (X) (red)
+ |
+ |
+ v
+ (Z) (blue)
+ */
+
+export const vector = (point: Point3d): Vector3 => {
+  return [point.x, point.y, point.z] as unknown as Vector3;
+};
+
+export const convert2dTo3d = (canvas: Point2d): Point3d => {
+  return {
+    x: canvas.x,
+    y: 0,
+    z: canvas.y,
+  };
+};
+
+export const convert3dTo2d = (threeJsPoint: Point3d): Point2d => {
+  return {
+    x: threeJsPoint.x,
+    y: threeJsPoint.z,
+  };
+};
+
+export type Point2d = {
+  x: number;
+  y: number;
+};
+
+export type Point3d = {
+  x: number;
+  y: number;
+  z: number;
+};
+
 export const Drawing = () => {
   return (
     <div
@@ -35,13 +86,21 @@ export const Drawing = () => {
         border: "1px solid black",
       }}
     >
-      <Canvas id={DrawingId} style={{ height: "400px" }}>
+      <Canvas
+        id={DrawingId}
+        style={{ height: "400px" }}
+        orthographic
+        camera={{
+          zoom: 50,
+          position: [0, 1, 0],
+        }}
+      >
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Square position={[-1.2, 0, 0]} />
-        <Square position={[1.2, 0, 0]} />
-        <gridHelper />
-        <OrbitControls />
+        <Square position={vector(convert2dTo3d({ x: 0, y: 0 }))} />
+        <Square position={vector(convert2dTo3d({ x: 4, y: 0 }))} />
+        <gridHelper args={[30, 30]} />
+        <axesHelper />
       </Canvas>
     </div>
   );
--- src/components/placeholder/drawing/unit.spec.tsx
+++ src/components/placeholder/drawing/unit.spec.tsx
@@ -1,6 +1,10 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import ReactThreeTestRenderer from "@react-three/test-renderer";
+import {
+  convert2dTo3d,
+  convert3dTo2d,
+} from "@/components/placeholder/drawing/index";
 
 const Mesh = () => {
   const meshRef = useRef();
@@ -29,5 +33,38 @@ describe("Mesh Test", () => {
     });
 
     expect(renderer.scene.children[0].instance.rotation.x).toEqual(2);
+  });
+});
+
+describe("Converting coordinate systems", () => {
+  it("should convert from 2D to 3D", () => {
+    expect(convert2dTo3d({ x: 1, y: 10 })).toEqual({
+      x: 1,
+      y: 0,
+      z: 10,
+    });
+  });
+
+  it("should convert from 3D to 2D", () => {
+    expect(convert2dTo3d({ x: 1, y: 10 })).toEqual({
+      x: 1,
+      y: 0,
+      z: 10,
+    });
+  });
+
+  it("should convert from 3d to 2d and back", () => {
+    expect(convert2dTo3d(convert3dTo2d({ x: 1, y: 0, z: 10 }))).toEqual({
+      x: 1,
+      y: 0,
+      z: 10,
+    });
+  });
+
+  it("should convert from 2d to 3d and back", () => {
+    expect(convert3dTo2d(convert2dTo3d({ x: 1, y: 10 }))).toEqual({
+      x: 1,
+      y: 10,
+    });
   });
 });

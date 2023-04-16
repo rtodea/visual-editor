d073d76920f19768511f55a7a55af81bfcaf3c7e
Robert Todea <robert.todea@mai.ai>
2023-04-16 08:24:46 +0300

    Initial implementation of closest point

---
+++ src/components/drawing/closest-point/index.ts
@@ -0,0 +1,112 @@
+import { Point2d } from "@/components/drawing/models";
+
+type Vector2d = Point2d;
+
+export const dotProduct = (v1: Vector2d, v2: Vector2d): number => {
+  // This function should return the dot product of the two vectors.
+  // The dot product is the sum of the products of the corresponding
+  // entries of the two vectors.
+
+  return v1.x * v2.x + v1.y * v2.y;
+};
+
+export const vectorLength = (v: Vector2d): number => {
+  return Math.sqrt(v.x * v.x + v.y * v.y);
+};
+
+export const vectorFromPoints = (a: Point2d, b: Point2d): Vector2d => {
+  return { x: b.x - a.x, y: b.y - a.y };
+};
+
+export const cosineOfAngleBetweenVectors = (
+  v1: Vector2d,
+  v2: Vector2d
+): number => {
+  const dot: number = dotProduct(v1, v2);
+  const v1Length: number = vectorLength(v1);
+  const v2Length: number = vectorLength(v2);
+  return dot / (v1Length * v2Length);
+};
+
+export const projectedPointOnEdge = ([a, b]: [Point2d, Point2d], p: Point2d) => {
+  // This function should return the point on the line defined by `a` and `b`
+  // that is closest to `p`.
+  const v1: Vector2d = vectorFromPoints(a, b);
+  const v2: Vector2d = vectorFromPoints(a, p);
+
+  const cosineV1V2: number = cosineOfAngleBetweenVectors(v1, v2);
+  const lengthOfProjectedVector: number = cosineV1V2 * vectorLength(v2);
+  const v1Length = vectorLength(v1);
+  const deltaX = (lengthOfProjectedVector * v1.x) / v1Length;
+  const deltaY = (lengthOfProjectedVector * v1.y) / v1Length;
+
+  return {
+    x: a.x + deltaX,
+    y: a.y + deltaY,
+  };
+};
+
+export const isProjectionOnEdge = (
+  pos: Point2d,
+  edge: [Point2d, Point2d]
+): boolean => {
+  const v1 = vectorFromPoints(edge[0], edge[1]);
+  const v2 = vectorFromPoints(pos, edge[1]);
+  const v1DotV2 = dotProduct(v1, v2);
+  const v1LengthSquared = v1.x * v1.x + v1.y * v1.y;
+  return 0 <= v1DotV2 && v1DotV2 < v1LengthSquared;
+};
+
+export const polygonToEdges = (poly: Point2d[]): [Point2d, Point2d][] => {
+  return poly.map((point: Point2d, index: number) => {
+    return [point, poly[(index + 1) % poly.length]];
+  });
+};
+
+export const pointDistance = (point1: Point2d, point2: Point2d): number => {
+  const dx = point1.x - point2.x;
+  const dy = point1.y - point2.y;
+  return Math.sqrt(dx * dx + dy * dy);
+};
+
+export const closestPointInPolygon = (
+  poly: Point2d[],
+  pos: Point2d
+): Point2d | undefined => {
+  // This function should return the closest point to `pos` that is
+  // inside the polygon defined by `poly`.
+  const edges = polygonToEdges(poly);
+  const edgesWithClosestPoints = edges.filter((edge) =>
+    isProjectionOnEdge(pos, edge)
+  );
+
+  let closestPointOnEdge: Point2d | undefined;
+  let closestPointDistance = Number.MAX_VALUE;
+  for (const edge of edgesWithClosestPoints) {
+    const projectedPoint = projectedPointOnEdge(edge, pos);
+    const projectedPointDistance = pointDistance(projectedPoint, pos);
+    if (projectedPointDistance < closestPointDistance) {
+      closestPointDistance = projectedPointDistance;
+      closestPointOnEdge = projectedPoint;
+    }
+  }
+
+  let closestPolygonVertex: Point2d | undefined;
+  let closestPolygonVertexDistance = Number.MAX_VALUE;
+  for (const vertex of poly) {
+    const vertexDistance = pointDistance(vertex, pos);
+    if (vertexDistance < closestPolygonVertexDistance) {
+      closestPolygonVertexDistance = vertexDistance;
+      closestPolygonVertex = vertex;
+    }
+  }
+
+  if (!closestPointOnEdge) {
+    return closestPolygonVertex;
+  }
+  if (closestPointDistance < closestPolygonVertexDistance) {
+    return closestPointOnEdge;
+  } else {
+    return closestPolygonVertex;
+  }
+};
---
+++ src/components/drawing/closest-point/unit.spec.ts
@@ -0,0 +1,61 @@
+import { describe, expect, it } from "@jest/globals";
+import {
+  closestPointInPolygon,
+  dotProduct,
+  projectedPointOnEdge,
+  vectorLength,
+} from "@/components/drawing/closest-point/index";
+
+describe("closest point logic", () => {
+  it("should return the closest point", () => {
+    expect(
+      closestPointInPolygon(
+        [
+          { x: 0, y: 0 },
+          { x: 10, y: 0 },
+          { x: 10, y: 10 },
+          { x: 0, y: 10 },
+        ],
+        { x: 15, y: 5 }
+      )
+    ).toEqual({ x: 10, y: 5 });
+  });
+});
+
+describe("dot product", () => {
+  it("should return the dot product of two vectors", () => {
+    expect(dotProduct({ x: 1, y: 2 }, { x: 3, y: 4 })).toEqual(11);
+  });
+});
+
+describe("line length", () => {
+  it("should return the length of a line", () => {
+    expect(vectorLength({ x: 3, y: 4 })).toEqual(5);
+  });
+});
+
+describe("projected point on line", () => {
+  it("should return the projected point on a line #1", () => {
+    expect(
+      projectedPointOnEdge(
+        [
+          { x: 0, y: 0 },
+          { x: 10, y: 0 },
+        ],
+        { x: 5, y: 5 }
+      )
+    ).toEqual({ x: 5, y: 0 });
+  });
+
+  it("should return the projected point on a line #2", () => {
+    expect(
+      projectedPointOnEdge(
+        [
+          { x: 0, y: 0 },
+          { x: 10, y: 0 },
+        ],
+        { x: 5, y: -5 }
+      )
+    ).toEqual({ x: 5, y: 0 });
+  });
+});

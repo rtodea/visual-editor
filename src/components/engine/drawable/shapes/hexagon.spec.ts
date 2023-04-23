9c423857679bfba373a7cb46f8257db04865fae6
Robert Todea <robert.todea@mai.ai>
2023-04-23 09:48:55 +0300

    Revert "Disable import error tests"
    This reverts commit ce6ca1eb89416f34f99e58d024782c82e1926206.

--- src/components/engine/drawable/shapes/hexagon.spec.ts
+++ src/components/engine/drawable/shapes/hexagon.spec.ts
@@ -1,9 +1,8 @@
-import { describe, expect, xit } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
 import { hexagonCenterToVertexList } from "@/components/engine/drawable/shapes/hexagon";
 
 describe("Hexagon from center to vertices", () => {
-  // TODO: fix weird import error preventing these tests to run
-  xit("should return a list of 6 vertices", () => {
+  it("should return a list of 6 vertices", () => {
     const vertexList = hexagonCenterToVertexList({ x: 0, y: 0, z: 0 });
     expect(vertexList.length).toEqual(6);
     expect(vertexList).toEqual([]);
--- src/components/engine/drawable/shapes/square.spec.ts
+++ src/components/engine/drawable/shapes/square.spec.ts
@@ -1,8 +1,8 @@
-import { describe, expect, xit } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
 import { squareCenterToVertexList } from "@/components/engine/drawable/shapes/square";
 
 describe("Square from center to vertices", () => {
-  xit("should return a list of 4 vertices", () => {
+  it("should return a list of 4 vertices", () => {
     const vertexList = squareCenterToVertexList({ x: 0, y: 0, z: 0 });
     expect(vertexList.length).toEqual(4);
     expect(vertexList).toEqual([]);
